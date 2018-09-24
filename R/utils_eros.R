313b085c7ec777e8b33175e9a9d538b2fbd58baa
Konstantinos Vantas <kon.vantas@gmail.com>
2018-09-24 09:13:52 +0300

    #1 fix reported total energy in utils_eros

--- R/utils_eros.R
+++ R/utils_eros.R
@@ -104,8 +104,8 @@ erosivity <- function(hyet, time_step, en_equation) {
     cum_prec = sum(.data$prec),
     max_i15 = max(.data$prec15) * 4,
     max_i30 = max(.data$prec30) * 2,
-    total_energy = sum(.data$energy, na.rm = TRUE),
-    erosivity = sum(.data$energy * .data$prec, na.rm = TRUE) * .data$max_i30,
+    total_energy = sum(.data$energy * .data$prec, na.rm = TRUE),
+    erosivity = total_energy * .data$max_i30,
     eros_density = .data$erosivity / .data$cum_prec
   )
   # remove extract_storm
